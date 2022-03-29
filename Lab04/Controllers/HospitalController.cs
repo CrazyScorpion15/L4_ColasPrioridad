9520e02f3d3248c4ee65d53d52e49c23ec81af10
SARUCA1 <santiagoruizc@gmail.com>
2022-03-28 18:12:05 -0600

    Calculo prio

--- Lab04/Controllers/HospitalController.cs
+++ Lab04/Controllers/HospitalController.cs
@@ -11,7 +11,6 @@ namespace Lab04.Controllers
 {
     public class HospitalController : Controller
     {
-        int prioridad = 0;
         // GET: HospitalController
         public ActionResult Index()
         {
@@ -48,67 +47,7 @@ namespace Lab04.Controllers
         {
             try
             {
-                //Sexo
-                if (collection["Sexo"] == "Hombre")
-                {
-                    prioridad += 3;
-                }
-                else if(collection["Sexo"] == "Mujer")
-                {
-                    prioridad += 5;
-                }
-                //Edad
-                if(collection["Edad"] == "70+")
-                {
-                    prioridad += 10;
-                }
-                else if(collection["Edad"] == "50-69")
-                {
-                    prioridad += 8;
-                }
-                else if (collection["Edad"] == "18-49")
-                {
-                    prioridad += 3;
-                }
-                else if (collection["Edad"] == "6-17")
-                {
-                    prioridad += 5;
-                }
-                else if (collection["Edad"] == "0-5")
-                {
-                    prioridad += 8;
-                }
-                //Especializacion
-                if (collection["Especializacion"] == "Traumatologia (Interna)")
-                {
-                    prioridad += 3;
-                }
-                else if (collection["Especializacion"] == "Traumatologia (Expuesta)")
-                {
-                    prioridad += 8;
-                }
-                else if (collection["Especializacion"] == "Ginecologia")
-                {
-                    prioridad += 5;
-                }
-                else if (collection["Especializacion"] == "Cardiologia")
-                {
-                    prioridad += 10;
-                }
-                else if (collection["Especializacion"] == "Neumologia")
-                {
-                    prioridad += 8;
-                }
-                //Ingreso
-                if (collection["Ingreso"] == "Ambulancia")
-                {
-                    prioridad += 5;
-                }
-                else if (collection["Ingreso"] == "Asistido")
-                {
-                    prioridad += 3;
-                }
-                var Informacion = HospitalModel.Save(new HospitalModel
+                var Informacion = HospitalModel.CalculoPrio(new HospitalModel
                 {
                     Nombre = collection["Nombre"],
                     Apellido = collection["Apellido"],
@@ -117,7 +56,6 @@ namespace Lab04.Controllers
                     Edad = collection["Edad"],
                     Especializacion = collection["Especializacion"],
                     Ingreso = collection["Ingreso"],
-                    Prioridad = prioridad,
                 });
                 return RedirectToAction(nameof(Index));
             }
