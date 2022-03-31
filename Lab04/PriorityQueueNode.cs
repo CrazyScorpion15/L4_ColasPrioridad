bfde0aff4ebede0eba83ecd53243679b9248700f
SARUCA1 <santiagoruizc@gmail.com>
2022-03-30 21:25:32 -0600

    Cola

--- Lab04/PriorityQueueNode.cs
+++ Lab04/PriorityQueueNode.cs
@@ -8,6 +8,7 @@ namespace Lab04
     public class PriorityQueueNode<T>
     {
         public int Priority { get; set; }
+        public int Pos { get; set; }
         public PriorityQueueNode<T> izq { get; set; }
         public PriorityQueueNode<T> der { get; set; }
         public T Object { get; set; }
